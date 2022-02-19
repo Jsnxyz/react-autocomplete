50319fd9c93c3ce241b23ab13ee8a783a9b118dc
Jason Andrade <ajasonandrade@gmail.com>
2022-02-19 15:43:11 +0100

    Responsive fix and better error message

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -4,10 +4,10 @@ import styles from './Results.module.css';
 const Results = (props) => {
     const renderedElement = props.result.url ? (
         <div className={styles.result} role="result">
-            <img src={props.result.image} alt={props.result.name} />
+            <img className={styles.result__img} src={props.result.image} alt={props.result.name} />
             <a target="_blank" title={`Click for more details on ${props.result.name}`} className={styles.result__link} href={props.result.url}>{props.result.name}</a>
         </div>
-    ) : <p aria-label="alert" className={styles.pokemon__nfound}>We didn't find {props.result.name}. Maybe someday if we explore more, we'd find it actually exists. 😆</p>
+    ) : <p aria-label="alert" className={styles.pokemon__nfound}>We didn't find {props.result.name}. It could simply be an error in the network or maybe someday if we explore more, we'd find it actually exists. 😆</p>
     return renderedElement;
 }
 
